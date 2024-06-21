df5975dc2b33dc6e5c144940ea7d92d6bb3a6e0e
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-22 04:13:13 +0500

    Начато создание Transfer Microservice.

---
+++ TransferSevice/src/test/java/com/kamenskiy/io/transfers/TransferSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamenskiy.io.transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
