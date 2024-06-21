30da56249f2ec75c99c3adce8960ea5546d76106
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-22 04:36:45 +0500

    Реализация сервиса TransferServiceImpl.

--- TransferSevice/src/main/java/com/kamenskiy/io/transfers/service/TransferServiceImpl.java
+++ TransferSevice/src/main/java/com/kamenskiy/io/transfers/service/TransferServiceImpl.java
@@ -4,12 +4,15 @@ import com.kamenskiy.io.transfers.model.TransferRestModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class TransferServiceImpl implements TransferService  {
+public class TransferServiceImpl implements TransferService {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     private KafkaTemplate<String, Object> kafkaTemplate;
     private Environment environment;
@@ -22,11 +25,39 @@ public class TransferServiceImpl implements TransferService  {
     }
 
     @Override
-    public boolean transfer(TransferRestModel productPaymentRestModel) {
+    public boolean transfer(TransferRestModel transferRestModel) {
+        WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
+        DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
 
+        try {
+            kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
+                    withdrawalEvent);
+            LOGGER.info("Sending withdrawal event to withdrawal topic");
 
+            //Business logic that causes and error
+            callRemoteService();
 
+            kafkaTemplate.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"),
+                    depositEvent);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
 
-        return false;
+        return true;
+    }
+
+    private ResponseEntity<String> callRemoteService() throws Exception {
+        String requestUrl;
+        ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+
+        if (response.getStatusCode().value() == HttpStatus.SERVICE_UNAVAILABLE.value()) {
+            throw new Exception("Destination Microservice is not available");
+        }
+        if (response.getStatusCode().value() == HttpStatus.OK.value()) {
+            LOGGER.info("Destination Microservice is available and received response from mock service");
+        }
     }
 }
