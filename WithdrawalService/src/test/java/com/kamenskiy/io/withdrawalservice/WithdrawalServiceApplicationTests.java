e300ba706243cf0a4655bed3f9b723d0e9b1b10e
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-22 17:03:07 +0500

    Разработка Консьюмера WithdrawalService.

---
+++ WithdrawalService/src/test/java/com/kamenskiy/io/withdrawalservice/WithdrawalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamenskiy.io.withdrawalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
